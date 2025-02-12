fa5bef31fe3f9cd5fc53c79d029bd591e0251170
jacekMe <jacekmeres@gmail.com>
2025-02-12 20:51:56 +0100

    przykłady z NULL

---
+++ 2.czytanie_danych/null.sql
@@ -0,0 +1,45 @@
+-- praca z NULL - poniższe polecenia nie zadziałają
+SELECT *
+FROM pracownicy
+WHERE drugie_imie = ''
+-- WHERE drugie_imie = NULL 
+-- WHERE drugie_imie <> NULL 
+-- WHERE NULL = NULL
+-- WHERE 1 <> NULL
+;
+
+-- aby móc wyświetlać NULL trzeba użyć operatora
+SELECT *
+FROM pracownicy
+WHERE drugie_imie IS NULL
+-- WHERE drugie_imie IS NOT NULL
+;
+
+BEGIN TRANSACTION;
+
+-- przechowywanie NULL i ''(pusty string)
+INSERT INTO pracownicy (imie, drugie_imie, nazwisko, stanowisko, email, telefon) 
+  VALUES ('Jacek', '', 'Meres', 'analityk danych', 'j.meres@firma.pl', '111-253-874');
+     
+INSERT INTO pracownicy (imie, drugie_imie, nazwisko, stanowisko, email, telefon) 
+  VALUES ('Jacek', NULL, 'Meres', 'analityk danych', 'j.meres@firma.pl', '111-253-874');
+
+SELECT *
+FROM pracownicy
+WHERE drugie_imie IS NULL;
+
+SELECT *
+FROM pracownicy
+WHERE drugie_imie ='';
+
+-- COALESCE sprawdza dwie kolumny
+SELECT *
+FROM pracownicy
+WHERE COALESCE(drugie_imie, '') = '';
+
+ROLLBACK;
+
+-- NULL vs brak wyników
+SELECT *
+FROM pracownicy
+WHERE id_pracownika = 2;
